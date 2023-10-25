b2198485e1620d5fdeeba32cc676a066ac34a5c4
suhwan2004 <suhwan2004@gmail.com>
2023-10-25 15:50:46 +0900

    Feat: type 파일 생성
    프로젝트 내 사용되는 types 폴더 생성 및 type 파일 생성

---
+++ frontend/src/types/type.d.ts
@@ -0,0 +1,18 @@
+declare module '*.ttf';
+
+declare module '*.png' {
+  const value: any;
+  export = value;
+}
+declare module '*.svg' {
+  const value: any;
+  export = value;
+}
+declare module '*.jpeg' {
+  const value: any;
+  export = value;
+}
+declare module '*.gif' {
+  const value: any;
+  export = value;
+}
