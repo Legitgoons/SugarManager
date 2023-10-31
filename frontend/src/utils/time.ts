d56b09977078fa2fede21484bbbe64e4f3cadbcc
suhwan2004 <suhwan2004@gmail.com>
2023-10-31 14:28:44 +0900

    Feat : Date 객체를 YYYYMMDD로 변환하는 format8Date 함수 추가

--- frontend/src/utils/time.ts
+++ frontend/src/utils/time.ts
@@ -1,11 +1,26 @@
 import weekDayArr from '@/config/weekConfig';
 
-function getMonthObj(year: number, month: number) {
+interface GetMonthObjProps {
+  year: number;
+  month: number;
+  monthStatus: Record<string, boolean>;
+}
+
+function format8Date(date: Date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+
+  return `${year}${month}${day}`;
+}
+
+function getMonthObj({ year, month, monthStatus }: GetMonthObjProps) {
   const firstDay = new Date(year, month - 1, 1);
   const lastDay = new Date(year, month, 0);
 
   const monthInfo = [];
   let weekInfo = [];
+
   for (
     let day = new Date(firstDay);
     day <= lastDay;
@@ -14,7 +29,11 @@ function getMonthObj(year: number, month: number) {
     const numberDay = day.getDate();
     const weekDay = weekDayArr[day.getDay()];
 
-    weekInfo.push({ numberDay, weekDay });
+    weekInfo.push({
+      numberDay,
+      weekDay,
+      isMarked: monthStatus[format8Date(day)],
+    });
 
     if (day.getDay() === 6) {
       monthInfo.push(weekInfo);
@@ -30,9 +49,9 @@ function getMonthObj(year: number, month: number) {
     const firstNumberDay = week[0].numberDay;
     while (week.length < 7) {
       if (firstNumberDay === 1) {
-        week.unshift({ numberDay: 0, weekDay: ' ' });
+        week.unshift({ numberDay: 0, weekDay: ' ', isMarked: false });
       } else {
-        week.push({ numberDay: 0, weekDay: ' ' });
+        week.push({ numberDay: 0, weekDay: ' ', isMarked: false });
       }
     }
   });
@@ -40,11 +59,12 @@ function getMonthObj(year: number, month: number) {
   if (monthInfo.length === 5) {
     const arr = [];
     for (let i = 0; i < 7; i += 1) {
-      arr.push({ numberDay: 0, weekDay: ' ' });
+      arr.push({ numberDay: 0, weekDay: ' ', isMarked: false });
     }
     monthInfo.push(arr);
   }
 
   return monthInfo;
 }
+
 export default getMonthObj;
