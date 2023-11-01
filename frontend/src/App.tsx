d02c43b6f4c411b20e4b641660639124b093759b
suhwan2004 <suhwan2004@gmail.com>
2023-11-01 18:29:04 +0900

    Fix : redux 세팅 주석 해제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components/native';
-// import { Provider as ReduxProvider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import SplashScreen from 'react-native-splash-screen';
 import theme from './styles/theme';
-// import { persistor, store } from './redux/store/storeConfig';
+import { persistor, store } from './redux/store/storeConfig';
 import { SigninScreen, HomeScreen } from './screens';
 
 /* @Todo : 현재, Slice가 없어서 ReduxProvider의 Store를 넣는 부분이 에러가 나서 임시 주석 처리했음.
@@ -24,24 +24,24 @@ export default function App() {
   });
   return (
     <ThemeProvider theme={theme}>
-      {/* <ReduxProvider store={store}>
-        <PersistGate loading={null} persistor={persistor}> */}
-      <QueryClientProvider client={queryClient}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Signin"
-              component={SigninScreen}
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen name="Home" component={HomeScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </QueryClientProvider>
-      {/* </PersistGate>
-      </ReduxProvider> */}
+      <ReduxProvider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <QueryClientProvider client={queryClient}>
+            <NavigationContainer>
+              <Stack.Navigator>
+                <Stack.Screen
+                  name="Signin"
+                  component={SigninScreen}
+                  options={{
+                    headerShown: false,
+                  }}
+                />
+                <Stack.Screen name="Home" component={HomeScreen} />
+              </Stack.Navigator>
+            </NavigationContainer>
+          </QueryClientProvider>
+        </PersistGate>
+      </ReduxProvider>
     </ThemeProvider>
   );
 }
