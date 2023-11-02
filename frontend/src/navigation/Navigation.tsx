ee0de10e0a67609d174afb35509fd1f0ff4d41c0
suhwan2004 <suhwan2004@gmail.com>
2023-11-02 17:10:04 +0900

    Feat : Navigation 로직 수정
    App.tsx 내에서 Navigation 로직을 분리
    각 페이지에 React.Suspense 적용

---
+++ frontend/src/navigation/Navigation.tsx
@@ -0,0 +1,35 @@
+import React, { Suspense } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { HomeScreen, SigninScreen } from '@/screens';
+import { useSelector } from 'react-redux';
+import { selectUser } from '@/redux/slice/userSlice';
+import Spinner from '@/components/atoms/Spinner';
+
+function SuspenseHomeScreen() {
+  return (
+    <Suspense fallback={<Spinner />}>
+      <HomeScreen />
+    </Suspense>
+  );
+}
+
+export default function Navigation({ Stack }: { Stack: any }) {
+  const { isSignin } = useSelector(selectUser);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {isSignin ? (
+          <Stack.Screen name="Home" component={SuspenseHomeScreen} />
+        ) : (
+          <Stack.Screen
+            name="Signin"
+            component={SigninScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
