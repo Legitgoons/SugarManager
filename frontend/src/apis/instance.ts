fcf26c55eb4cc5566294d8c89009a33ddc2cad7a
suhwan2004 <suhwan2004@gmail.com>
2023-11-02 17:11:08 +0900

    Fix : instance response interceptors 내에 에러 상황에서 로그인 페이지로 이동하도록 수정

--- frontend/src/apis/instance.ts
+++ frontend/src/apis/instance.ts
@@ -1,3 +1,4 @@
+import { navigate } from '@/navigation/NavigationService';
 import { store, RootState } from '@/redux/store/storeConfig';
 import axios from 'axios';
 
@@ -22,7 +23,29 @@ instance.interceptors.request.use(
 );
 
 instance.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    const { data } = response;
+
+    if (data?.error) {
+      /*
+        1. token Refreshing ->  token refresh -> re-request
+          - U003(JWT 만료)
+        2. can't token Refreshing -> navigate signin
+          - U001(인증 실패), U002(권한 없음)
+      */
+      switch (data.error.code) {
+        case 'U003':
+          console.log('need TokenRefreshing');
+          break;
+        case 'U001' || 'U002':
+          navigate('Signin');
+          break;
+        default:
+          break;
+      }
+    }
+    return response;
+  },
   async (error) => error
 );
 
