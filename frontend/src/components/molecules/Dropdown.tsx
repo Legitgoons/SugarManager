8e068d82317110a7bd72de2d3cf4d40d4e090cf0
이의찬 <cksxkr5193@naver.com>
2023-11-02 15:28:35 +0900

    Feat: Dropdown 컴포넌트 구현

---
+++ frontend/src/components/molecules/Dropdown.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { Pressable, FlatList } from 'react-native';
+import BlackRightArrowIcon from '@/assets/icon/BlackRightArrowIcon.svg';
+import styled from 'styled-components/native';
+
+interface DropdownProps {
+  placeholder: string;
+  list: Array<{ id: string; value: string }>;
+  selectItem: { id: string; value: string } | null;
+  setSelectItem: React.Dispatch<
+    React.SetStateAction<{ id: string; value: string } | null>
+  >;
+}
+
+/** 공통 Dropdown Component
+@param {String} placeholder : Dropdown의 값 미선택 시 나오는 placeholder 지정
+@param {Array<{ id: string; value: string }>} list : 드롭다운 클릭 시 나오는 선택 가능한 리스트들. id는 key를 대응하며, value는 실제 보이는 값
+@param {{ id: string; value: string } | null} selectItem : 현재 선택된 값. null 가능성 존재
+@param {React.Dispatch<React.SetStateAction<{ id: string; value: string } | null>>} setSelectItem selectItem의 상태를 변경해주는 함수
+@returns {JSX.Element} 컴포넌트 반환 */
+
+const DropdownContainer = styled.View`
+  position: relative;
+`;
+const DropdownButton = styled(Pressable)`
+  border-width: 1px;
+  width: 120px;
+  height: 40px;
+  border-color: ${(props) => props.theme.colors.secondary};
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const DropdownText = styled.Text``;
+
+const BlackArrowIcon = styled(BlackRightArrowIcon)<{ modalVisible: boolean }>`
+  justify-self: end;
+  transform: ${(props) => {
+    if (props.modalVisible === true) {
+      return 'rotate(270deg)';
+    }
+    return 'rotate(90deg)';
+  }};
+`;
+
+const DropdownListContainer = styled.View`
+  position: absolute;
+  width: 120px;
+  top: 100%;
+  border-radius: 10px;
+  overflow: hidden;
+`;
+const ListItem = styled(Pressable)`
+  width: 120px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+
+const ListItemText = styled.Text``;
+
+export default function Dropdown({
+  placeholder,
+  list,
+  selectItem,
+  setSelectItem,
+}: DropdownProps) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const renderItem = ({ item }: { item: { id: string; value: string } }) => (
+    <ListItem
+      onPress={() => {
+        setSelectItem(item);
+        setModalVisible(false);
+      }}
+    >
+      <ListItemText>{item.value}</ListItemText>
+    </ListItem>
+  );
+
+  return (
+    <DropdownContainer>
+      <DropdownButton onPress={() => setModalVisible(!modalVisible)}>
+        <DropdownText>
+          {selectItem ? selectItem.value : placeholder}
+        </DropdownText>
+        <BlackArrowIcon width={20} height={20} modalVisible={modalVisible} />
+      </DropdownButton>
+      {modalVisible && (
+        <DropdownListContainer>
+          <FlatList
+            data={list}
+            renderItem={renderItem}
+            keyExtractor={(item: { id: string; value: string }) => item.id}
+          />
+        </DropdownListContainer>
+      )}
+    </DropdownContainer>
+  );
+}
