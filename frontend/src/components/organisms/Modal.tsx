a9d08cb53e8d39488821f0e4f240a4c93a719b50
이의찬 <cksxkr5193@naver.com>
2023-11-07 14:25:55 +0900

    Feat: Modal 컴포넌트 구현

---
+++ frontend/src/components/organisms/Modal.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Modal } from 'react-native';
+import styled from 'styled-components/native';
+import { rWidth, rHeight } from '@/utils/style';
+import Overlay from '@/styles/View';
+import { TypographyType } from '@/styles/theme';
+import TwinButtonGroup from '../molecules/TwinButtonGroup';
+
+interface DefaultModalProps {
+  modalVisible: boolean;
+  setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  mainTitle: string;
+  children: React.ReactNode;
+  leftBtnTitle: string;
+  onLeftPress: () => void;
+  rightBntTitle: string;
+  onRightPress: () => void;
+}
+
+/** DefaultModal
+ * @param {Boolean} modalVisible 모달 띄울지 여부
+ * @param {React.Dispatch<React.SetStateAction<boolean>>} setModalVisible
+ * @param {String} mainTitle 모달 상단 Title
+ * @param {React.ReactNode} children 모달 내부 내용
+ * @param {String} leftBtnTitle 모달 좌측 버튼 Title
+ * @param {() => void} onLeftPress 모달 좌측 버튼 callback함수
+ * @param {String} rightBntTitle 모달 우측 버튼 Title
+ * @param {() => void} onRightPress 모달 우측 버튼 callback함수
+ */
+
+const ModalContainer = styled.View`
+  width: ${rWidth(360)}px;
+  height: ${rHeight(496)}px;
+  z-index: 10;
+  border-width: 1px;
+  border-radius: 10px;
+  background-color: white;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalBox = styled.View`
+  width: ${rWidth(320)}px;
+  height: ${rHeight(328)}px;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+const MainTitleWrapper = styled.Text<{ fontSize: keyof TypographyType }>`
+  color: ${({ theme }) => theme.colors.black};
+  ${({ fontSize, theme }) => theme.typography[fontSize]};
+  width: 100%;
+  height: ${rHeight(32)}px;
+  background-color: white;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default function DefaultModal({
+  modalVisible,
+  setModalVisible,
+  mainTitle,
+  children,
+  leftBtnTitle,
+  onLeftPress,
+  rightBntTitle,
+  onRightPress,
+}: DefaultModalProps) {
+  return (
+    <Modal
+      animationType="none"
+      transparent
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible(false)}
+    >
+      <Overlay>
+        <ModalContainer>
+          <ModalBox>
+            <MainTitleWrapper fontSize="h1b">{mainTitle}</MainTitleWrapper>
+            {children}
+            <TwinButtonGroup
+              leftTitle={leftBtnTitle}
+              onLeftPress={onLeftPress}
+              rightTitle={rightBntTitle}
+              onRightPress={onRightPress}
+            />
+          </ModalBox>
+        </ModalContainer>
+      </Overlay>
+    </Modal>
+  );
+}
