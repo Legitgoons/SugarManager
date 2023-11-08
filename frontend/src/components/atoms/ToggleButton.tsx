80875ff2c5ca54fc9a7c3e812da998fda39e0f08
이의찬 <cksxkr5193@naver.com>
2023-11-08 13:54:29 +0900

    Feat: ToggleButton, Toggle 컴포넌트 구현

---
+++ frontend/src/components/atoms/ToggleButton.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import DefaultPressable from '@/styles/Pressable';
+import DefaultPressableProps from '@/types/pressable';
+import styled from 'styled-components/native';
+import { rWidth, rHeight } from '@/utils/style';
+
+interface ToggleButtonProps extends DefaultPressableProps {
+  isActive: boolean;
+}
+
+const ToggleButtonWrapper = styled(DefaultPressable)<ToggleButtonProps>`
+  width: ${rWidth(148)}px;
+  height: ${rHeight(40)}px;
+  background-color: ${({ isActive, theme }) =>
+    isActive ? theme.colors.white : theme.colors.tertiary};
+`;
+
+const ButtonTitleWrapper = styled.Text<{ isActive: boolean }>`
+  ${({ theme }) => theme.typography.p2b}
+  color : ${({ isActive, theme }) =>
+    isActive ? theme.colors.black : theme.colors.primary};
+`;
+
+/**
+ * @param {boolean} isActive 활성화 되었는지 나타내는 boolean 변수
+ * @returns {JSX.Element} Toggle 내부 Button Component
+ */
+
+export default function ToggleButton({
+  onPress,
+  isActive,
+  title,
+}: ToggleButtonProps) {
+  return (
+    <ToggleButtonWrapper onPress={onPress} isActive={isActive}>
+      <ButtonTitleWrapper isActive={isActive}>{title}</ButtonTitleWrapper>
+    </ToggleButtonWrapper>
+  );
+}
