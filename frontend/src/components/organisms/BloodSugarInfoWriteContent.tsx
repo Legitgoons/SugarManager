edab2156f720d6111aa87e0dc021ec3c237722ac
이의찬 <cksxkr5193@naver.com>
2023-11-09 17:45:32 +0900

    BloodSugarInfoWriteContent 컴포넌트 구현

---
+++ frontend/src/components/organisms/BloodSugarInfoWriteContent.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { rWidth, rHeight } from '@/utils/style';
+import { DatePickerProps } from '@/types/datePicker';
+import InputLine from '../molecules/InputLine';
+import TextDatePicker from '../molecules/TextDatePicker';
+
+const WriteContentBox = styled.View`
+  width: ${rWidth(320)}px;
+  height: ${rHeight(240)}px;
+  justify-content: space-between;
+`;
+
+const DatePickerBox = styled.View`
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const BoxTextWrapper = styled.Text`
+  ${({ theme }) => theme.typography.p2r};
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+interface BloodSugarInfoWriteContentProps extends DatePickerProps {
+  bloodSugar: string;
+  setBloodSugar: React.Dispatch<React.SetStateAction<string>>;
+  issue: string;
+  setIssue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function BloodSugarInfoWriteContent({
+  bloodSugar,
+  setBloodSugar,
+  issue,
+  setIssue,
+  date,
+  setDate,
+  mode,
+}: BloodSugarInfoWriteContentProps) {
+  return (
+    <WriteContentBox>
+      <InputLine
+        onChangeText={setBloodSugar}
+        value={bloodSugar}
+        placeholder="혈당을 입력해주세요"
+        unit="mg/dL"
+      />
+      <InputLine
+        onChangeText={setIssue}
+        value={issue}
+        placeholder="특이사항을 입력해주세요"
+      />
+      <DatePickerBox>
+        <BoxTextWrapper>기록일시</BoxTextWrapper>
+        <TextDatePicker date={date} setDate={setDate} mode={mode} />
+      </DatePickerBox>
+    </WriteContentBox>
+  );
+}
