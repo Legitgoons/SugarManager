b55c673d797a443bc824a9910260deb3d190cbae
이의찬 <cksxkr5193@naver.com>
2023-11-09 14:50:35 +0900

    Feat: TextDatePicker Component 구현

---
+++ frontend/src/components/molecules/TextDatePicker.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { rWidth } from '@/utils/style';
+import styled from 'styled-components/native';
+import BlackRightArrowIcon from '@/assets/icon/BlackRightArrowIcon.svg';
+import { DatePickerProps } from '@/types/datePicker';
+import DatePickerModal from '../atoms/DatePickerModal';
+import DatePickerFormat from '../atoms/DatePickerFormat';
+
+const TextDatePickerButton = styled.Pressable`
+  width: ${rWidth(128)}px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const BlackArrowIcon = styled(BlackRightArrowIcon)``;
+
+interface TextDatePickerProps extends DatePickerProps {}
+
+/**
+ * @returns {JSX.Element} 날짜와 시간을 표시해주는
+ */
+
+export default function TextDatePicker({
+  open,
+  setOpen,
+  date,
+  setDate,
+  mode,
+}: TextDatePickerProps) {
+  return (
+    <TextDatePickerButton onPress={() => setOpen(true)}>
+      <DatePickerFormat color="b4" date={date} mode={mode} typography="p2r" />
+      <DatePickerModal
+        open={open}
+        setOpen={setOpen}
+        date={date}
+        setDate={setDate}
+        mode={mode}
+      />
+      <BlackArrowIcon />
+    </TextDatePickerButton>
+  );
+}
