a6de385ebd43d1af8f3c530d29c6dc64cf1b920c
suhwan2004 <suhwan2004@gmail.com>
2023-11-09 10:55:27 +0900

    Fix : HomeHeader 스타일링 수정

--- frontend/src/components/organisms/HomeHeader.tsx
+++ frontend/src/components/organisms/HomeHeader.tsx
@@ -1,25 +1,40 @@
 import DefaultText from '@/styles/Text';
 import React from 'react';
 import styled from 'styled-components/native';
-import Dropdown from '@/assets/icon/BlackHambugerIcon.svg';
-import { rHeight, rWidth } from '@/utils/style';
+import { rWidth } from '@/utils/style';
+import { HomeDropdownItem } from '@/types/homeDropdown';
+import { postSignout } from '@/apis/auth';
+import { persistor } from '@/redux/store/storeConfig';
+import useRouter from '@/hooks/useRouter';
+import HomeDropDown from '../molecules/HomeDropdown';
 
 const HeaderLogoWrapper = styled(DefaultText)``;
 
 const HomeHeaderBox = styled.View`
-  width: 100%;
+  width: ${rWidth(320)}px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  flex-direction: row;
 `;
 
 export default function HomeHeader() {
+  const homeDropdownItemList: HomeDropdownItem[] = [
+    { name: '계정 관리', link: 'Profile' },
+    { name: '알람 설정', link: 'Alarm' },
+  ];
+  const router = useRouter();
+  const handleSignOut = async () => {
+    await postSignout();
+    await persistor.purge();
+    router.navigate('Signin');
+  };
   return (
     <HomeHeaderBox>
       <HeaderLogoWrapper color="b4" typography="t3">
         관리하당
       </HeaderLogoWrapper>
-      <Dropdown width={rWidth(20)} height={rHeight(20)} />
+      <HomeDropDown items={homeDropdownItemList} signOut={handleSignOut} />
     </HomeHeaderBox>
   );
 }
