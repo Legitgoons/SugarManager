b29a08e883fe7bf0b76b164225330c136299f656
suhwan2004 <suhwan2004@gmail.com>
2023-11-09 10:53:22 +0900

    Feat : 카드 내 사용되는 진행 바 추가

---
+++ frontend/src/components/atoms/ProgressBar.tsx
@@ -0,0 +1,35 @@
+import extractNumber from '@/utils/number';
+import { rHeight } from '@/utils/style';
+import React from 'react';
+import styled from 'styled-components/native';
+
+const ProgressBarContainer = styled.View`
+  width: 100%;
+  height: ${rHeight(4)}px;
+  background-color: ${({ theme }) => theme.colors.tertiary};
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+const ProgressWrapper = styled.View<{ progress: number }>`
+  height: 100%;
+  width: ${({ progress }) => `${progress}%`};
+  background-color: ${({ theme }) => theme.colors.b4};
+  border-radius: 10px;
+`;
+interface ProgressBarProps {
+  current: string;
+  goal: string;
+}
+function ProgressBar({ current, goal }: ProgressBarProps) {
+  const currentNum = extractNumber(current);
+  const goalNum = extractNumber(goal);
+
+  return (
+    <ProgressBarContainer>
+      <ProgressWrapper progress={(currentNum / goalNum) * 100} />
+    </ProgressBarContainer>
+  );
+}
+
+export default ProgressBar;
