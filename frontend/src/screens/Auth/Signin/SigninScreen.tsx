a6d9d7b3c96b13ff4ecbb979980109a743924f38
suhwan2004 <suhwan2004@gmail.com>
2023-11-09 10:58:01 +0900

    Fix : 로그인 페이지 수정

--- frontend/src/screens/Auth/Signin/SigninScreen.tsx
+++ frontend/src/screens/Auth/Signin/SigninScreen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import OAuthButton from '@/components/atoms/OAuthButton';
 import TitleHeader from '@/components/molecules/TitleHeader';
 import DefaultScreenContainer from '@/styles/Container';
@@ -5,14 +6,13 @@ import React from 'react';
 import KakaoIcon from '@/assets/icon/kakaoIcon.svg';
 import styled from 'styled-components/native';
 import { login } from '@react-native-seoul/kakao-login';
-import postKakaoSignin from '@/apis/auth';
-import { setIsSignin, setKakaoToken, setToken } from '@/redux/slice/userSlice';
+import { postKakaoSignin } from '@/apis/auth';
+import { setKakaoToken, setProfile, setToken } from '@/redux/slice/userSlice';
 import { useDispatch } from 'react-redux';
-import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import showAlert from '@/utils/alert';
-import { RootStackParam } from '@/types/navigation';
 import { rHeight } from '@/utils/style';
+import getMyProfile from '@/apis/member';
+import useRouter from '@/hooks/useRouter';
 
 const SigninScreenContainer = styled(DefaultScreenContainer)`
   justify-content: flex-start;
@@ -22,12 +22,11 @@ const SigninScreenContainer = styled(DefaultScreenContainer)`
 
 export default function SigninScreen() {
   const dispatch = useDispatch();
-  const navigation = useNavigation<NativeStackNavigationProp<RootStackParam>>();
+  const router = useRouter();
   const signInWithKakao = async () => {
     try {
       const { accessToken: kakaoAccessToken, refreshToken: kakaoRefreshToken } =
         await login();
-
       dispatch(
         setKakaoToken({
           kakaoAccessToken,
@@ -36,30 +35,40 @@ export default function SigninScreen() {
       );
 
       const apiRes: any = await postKakaoSignin({
-        accessToken: kakaoAccessToken,
-        fcmToken: '1234',
+        access_token: kakaoAccessToken,
+        fcm_token: '1234',
       });
-
-      if (apiRes.data.error === null) {
-        const { accessToken, refreshToken } = apiRes.data.response;
+      if (apiRes.error === null) {
+        const { access_token, refresh_token } = apiRes.response;
         dispatch(
           setToken({
-            accessToken,
-            refreshToken,
+            accessToken: access_token,
+            refreshToken: refresh_token,
           })
         );
-        dispatch(setIsSignin(true));
-        navigation.navigate('Home');
       } else {
         showAlert({
           title: '로그인 실패',
-          content: apiRes.data.error.message,
+          content: apiRes.error.message,
           onOk: () => {},
         });
       }
 
-      return apiRes;
+      const myProfile = await getMyProfile();
+      if (myProfile.error === null) {
+        dispatch(setProfile({ ...myProfile.response }));
+        router.navigate('Home');
+      } else {
+        showAlert({
+          title: '로그인 실패',
+          content: myProfile.error.message,
+          onOk: () => {},
+        });
+      }
+
+      return null;
     } catch (e) {
+      console.log(e);
       showAlert({
         title: '로그인 실패',
         content: '네트워크 상태를 다시 확인해주세요!',
