36f900955af664d2b71050df868b3c77dd37a24a
이의찬 <cksxkr5193@naver.com>
2023-11-13 19:55:40 +0900

    Feat: BloodSugarScreen page 기능 추가 구현
    fetchBloodSugarData 함수를 이용해 데이터를 가져와 배열의 수만큼 BloodSugarContentCard 컴포넌트를 출력하도록 했습니다.
    handleScroll 함수를 이용해 페이지의 최하단에 도달할 경우 다음 페이지를 불러오도록 했습니다.
    또한 최상단일 경우 MainFillButton 컴포넌트가 렌더링되도록 했습니다.
    setStartDateSafe와 setEndDateSafe 함수를 이용해 날짜가 적절히 선택되도록 했습니다.

--- frontend/src/screens/BloodSugar/BloodSugarScreen.tsx
+++ frontend/src/screens/BloodSugar/BloodSugarScreen.tsx
@@ -1,29 +1,123 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { ScrollView } from 'react-native';
 import styled from 'styled-components/native';
+import { rHeight } from '@/utils';
+import { periodBloodSugar } from '@/apis/bloodSugar';
+import { BloodSugarResponseData } from '@/types/api/request/bloodSugar';
+import { useSelector } from 'react-redux';
+import { selectNavigation } from '@/redux/slice/navigationSlice';
+import useRouter from '@/hooks/useRouter';
 import BloodSugarContentCard from '@/components/organisms/BloodSugarContentCard';
 import DatePickerController from '@/components/organisms/DatePickerController';
+import MainFillButton from '@/components/atoms/MainFillButton';
 
-const BloodSugarContainer = styled.View``;
+const BloodSugarContainer = styled.View`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  gap: ${rHeight(30)}px;
+  align-items: center;
+`;
+
+const FillButtonWrapper = styled.View`
+  position: absolute;
+  bottom: 52px;
+  width: 100%;
+  align-items: center;
+`;
 
 export default function BloodSugarScreen() {
+  const router = useRouter();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const { nickname } = useSelector(selectNavigation);
+  const [bloodSugarData, setBloodSugarData] = useState<
+    BloodSugarResponseData[]
+  >([]);
+  const [page, setPage] = useState(0);
+  const [isTop, setIsTop] = useState(true);
+
+  const setStartDateSafe = (date: Date) => {
+    if (date > endDate) {
+      setStartDate(endDate);
+    } else {
+      setStartDate(date);
+    }
+  };
+
+  const setEndDateSafe = (date: Date) => {
+    const now = new Date();
+    if (date > now) {
+      setEndDate(now);
+    } else if (date < startDate) {
+      setEndDate(startDate);
+    } else {
+      setEndDate(date);
+    }
+  };
+
+  const fetchBloodSugarData = useCallback(async () => {
+    const data = await periodBloodSugar({
+      nickname,
+      startDate,
+      endDate,
+      page,
+    });
+    setBloodSugarData((prevData) => [...prevData, ...data.response]);
+  }, [nickname, startDate, endDate, page]);
+
+  useEffect(() => {
+    fetchBloodSugarData();
+  }, [fetchBloodSugarData]);
+
+  const handleScroll = (event: any) => {
+    const { nativeEvent } = event;
+    const { contentOffset, layoutMeasurement, contentSize } = nativeEvent;
+    const isScrolledToTop = contentOffset.y === 0;
+    const isScrolledToBottom =
+      contentOffset.y + layoutMeasurement.height >= contentSize.height;
+    setIsTop(isScrolledToTop);
+    if (isScrolledToBottom) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
   return (
     <BloodSugarContainer>
-      <DatePickerController
-        startDate={startDate}
-        setStartDate={setStartDate}
-        endDate={endDate}
-        setEndDate={setEndDate}
-      />
-      <BloodSugarContentCard
-        date="11/10"
-        count={1}
-        beforeNum={43}
-        beforeType="warning"
-        afterNum={112}
-        afterType="safety"
-      />
+      <ScrollView onScroll={handleScroll}>
+        <DatePickerController
+          startDate={startDate}
+          setStartDate={setStartDateSafe}
+          endDate={endDate}
+          setEndDate={setEndDateSafe}
+        />
+        {bloodSugarData.map(
+          (item) =>
+            item.bloodSugarBefore != null &&
+            item.bloodSugarAfter != null && (
+              <BloodSugarContentCard
+                key={item.time}
+                date={item.time}
+                count={item.count}
+                beforeNum={item.bloodSugarBefore}
+                beforeType="warning"
+                afterNum={item.bloodSugarAfter}
+                afterType="safety"
+              />
+            )
+        )}
+      </ScrollView>
+      {isTop && (
+        <FillButtonWrapper>
+          <MainFillButton
+            bgColor="b4"
+            title="혈당 등록하기"
+            onPress={() => {
+              router.navigate('BloodSugarWrite');
+            }}
+          />
+        </FillButtonWrapper>
+      )}
     </BloodSugarContainer>
   );
 }
