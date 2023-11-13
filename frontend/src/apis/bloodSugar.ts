be3df6de8ec3a8fdc5961eb1235e53fe93b1bd81
이의찬 <cksxkr5193@naver.com>
2023-11-13 19:34:54 +0900

    Feat: periodBloodSugar api 함수 구현
    기간 혈당 기록을 요청하는 함수입니다.

--- frontend/src/apis/bloodSugar.ts
+++ frontend/src/apis/bloodSugar.ts
@@ -1,10 +1,48 @@
 import { API_ENDPOINT } from '@env';
 import { store, RootState } from '@/redux/store/storeConfig';
-import { BloodSugarWriteData } from '@/types/api/request/bloodSugar';
+import {
+  BloodSugarWriteData,
+  periodBloodSugarProps,
+  BloodSugarApiResponse,
+} from '@/types/api/request/bloodSugar';
 
-export default async function saveBloodSugar(data: BloodSugarWriteData) {
+export async function periodBloodSugar({
+  nickname,
+  startDate,
+  endDate,
+  page,
+}: periodBloodSugarProps): Promise<BloodSugarApiResponse> {
   const state: RootState = store.getState();
   const { accessToken } = state.user;
+
+  const start = `${startDate.getFullYear()}-${
+    startDate.getMonth() + 1
+  }-${startDate.getDate()}`;
+  const end = `${endDate.getFullYear()}-${
+    endDate.getMonth() + 1
+  }-${endDate.getDate()}`;
+
+  const response = await fetch(
+    `${API_ENDPOINT}/bloodsugar/period/${nickname}/${start}/${end}/${page}`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+export async function saveBloodSugar(data: BloodSugarWriteData) {
+  const state: RootState = store.getState();
+  const { accessToken } = state.user;
+
   const response = await fetch(`${API_ENDPOINT}/bloodsugar/save`, {
     method: 'POST',
     headers: {
