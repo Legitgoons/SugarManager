3997075e93c0466c7425be5e9b8a11c60d9450c4
이의찬 <cksxkr5193@naver.com>
2023-11-13 10:05:11 +0900

    Fix: ColorSquare 컴포넌트 명 변경 및 colorSquareType 설정

--- frontend/src/components/atoms/ColorSquare.tsx
+++ frontend/src/components/atoms/ColorSquare.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import { rWidth, rHeight } from '@/utils/style';
+import { ColorSquareProps, colorSquareType } from '@/types/colorSquare';
 
-const SquareBoxWrapper = styled.View<{ type: 'safety' | 'warning' | 'danger' }>`
+const ColorSquarexWrapper = styled.View<{
+  type: colorSquareType;
+}>`
   width: ${rWidth(44)}px;
   height: ${rHeight(44)}px;
   display: flex;
@@ -22,26 +25,21 @@ const SquareBoxWrapper = styled.View<{ type: 'safety' | 'warning' | 'danger' }>`
     return props.theme.colors.b4;
   }};
 `;
-const BoxNumWrapper = styled.Text`
+const SquareNumWrapper = styled.Text`
   ${({ theme }) => theme.typographys.h3b};
   color: ${({ theme }) => theme.colors.white};
 `;
 
-interface SquareBoxProps {
-  num: number;
-  type: 'safety' | 'warning' | 'danger';
-}
-
-/** SquareBox Component
+/** ColorSquare Component
  * @param {Number} num Box에서 출력할 숫자
  * @param {'safety' | 'warning' | 'danger'} type 색을 출력하기 위해 입력받을 타입
  * @returns {JSX.Element} 정사각형 Box 컴포넌트
  */
 
-export default function SquareBox({ num, type }: SquareBoxProps) {
+export default function ColorSquare({ num, type }: ColorSquareProps) {
   return (
-    <SquareBoxWrapper type={type}>
-      <BoxNumWrapper>{num}</BoxNumWrapper>
-    </SquareBoxWrapper>
+    <ColorSquarexWrapper type={type}>
+      <SquareNumWrapper>{num}</SquareNumWrapper>
+    </ColorSquarexWrapper>
   );
 }
