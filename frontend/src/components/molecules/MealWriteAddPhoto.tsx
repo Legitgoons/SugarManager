db4a22d74dce2b9890922ad63f65d32166f60f25
이의찬 <cksxkr5193@naver.com>
2023-11-17 10:24:57 +0900

    Feat: MealWriteAddPhoto 컴포넌트 구현
    사진을 등록시키고, 등록된 사진을 보여주는 컴포넌트입니다.

--- frontend/src/components/molecules/MealWriteAddPhoto.tsx
+++ frontend/src/components/molecules/MealWriteAddPhoto.tsx
@@ -1,40 +1,70 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import ImagePicker from 'react-native-image-crop-picker';
+import { ScrollView, Pressable } from 'react-native';
 import { DefaultPressable, DefaultText } from '@/styles';
 import AddPhoto from '@/assets/icon/AddPhoto.svg';
 import { rWidth, rHeight } from '@/utils';
-import ImagePicker from 'react-native-image-crop-picker';
 
-const MealWriteAddPhotoBox = styled(DefaultPressable)``;
-
-const AddPhotoButton = styled.Pressable`
+const MealWriteAddPhotoBox = styled(DefaultPressable)`
   width: ${rWidth(320)}px;
-  height: ${rHeight(100)}px;
+  flex-direction: row;
+  justify-content: flex-start;
+`;
+
+const SelectedPhotoBox = styled.View`
+  width: 100%;
   flex-direction: row;
   justify-content: space-between;
 `;
-
-const AddPhotoIcon = styled(AddPhoto)``;
-const AddPhotoTextWrapper = styled(DefaultText)`
+const ImageScrollViewWrapper = styled.View`
   width: ${rWidth(240)}px;
+  overflow: hidden;
 `;
 
 const SelectedImage = styled.Image`
   width: ${rWidth(80)}px;
   height: ${rHeight(100)}px;
+  border-radius: ${rHeight(10)}px;
+`;
+
+const AddPhotoButton = styled.Pressable`
+  width: ${rWidth(80)}px;
+  height: ${rHeight(100)}px;
+  align-items: center;
+  justify-content: center;
+`;
+const AddPhotoIcon = styled(AddPhoto)``;
+
+const AddPhotoBox = styled.View`
+  width: ${rWidth(320)}px;
+  height: ${rHeight(100)}px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const AddPhotoTextWrapper = styled(DefaultText)`
+  width: ${rWidth(240)}px;
+  height: ${rHeight(60)}px;
+  align-items: center;
+  justify-content: center;
 `;
 
 interface MealWriteAddPhotoProps {
   selectedImages: string[];
   setSelectedImages: React.Dispatch<React.SetStateAction<string[]>>;
+  setExpandedImage: React.Dispatch<React.SetStateAction<string>>;
+  photoModalOpen: () => void;
 }
 
 export default function MealWriteAddPhoto({
   selectedImages,
   setSelectedImages,
+  setExpandedImage,
+  photoModalOpen,
 }: MealWriteAddPhotoProps) {
   const pickImage = () => {
-    console.log('클릭');
     ImagePicker.openPicker({
       multiple: true,
     }).then((images) => {
@@ -42,19 +72,37 @@ export default function MealWriteAddPhoto({
     });
   };
 
+  const openImage = (image: React.SetStateAction<string>) => {
+    setExpandedImage(image);
+    photoModalOpen();
+  };
+
   return (
-    <MealWriteAddPhotoBox onPress={pickImage}>
+    <MealWriteAddPhotoBox>
       {selectedImages.length > 0 ? (
-        selectedImages.map((image) => (
-          <SelectedImage key={image} source={{ uri: image }} />
-        ))
+        <SelectedPhotoBox>
+          <ImageScrollViewWrapper>
+            <ScrollView horizontal>
+              {selectedImages.map((image) => (
+                <Pressable key={image} onPress={() => openImage(image)}>
+                  <SelectedImage source={{ uri: image }} />
+                </Pressable>
+              ))}
+            </ScrollView>
+          </ImageScrollViewWrapper>
+          <AddPhotoButton onPress={pickImage}>
+            <AddPhotoIcon />
+          </AddPhotoButton>
+        </SelectedPhotoBox>
       ) : (
-        <AddPhotoButton>
-          <AddPhotoIcon />
+        <AddPhotoBox>
+          <AddPhotoButton onPress={pickImage}>
+            <AddPhotoIcon />
+          </AddPhotoButton>
           <AddPhotoTextWrapper color="primary" typography="p2r">
-            사진을 등록해서{'\n'}음식 정보를 찾아보세요!
+            사진을 등록해보세요!
           </AddPhotoTextWrapper>
-        </AddPhotoButton>
+        </AddPhotoBox>
       )}
     </MealWriteAddPhotoBox>
   );
