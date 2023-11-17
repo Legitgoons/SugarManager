58fc7b1c8b008e5a5036d89d25900a0f7544d6a1
이의찬 <cksxkr5193@naver.com>
2023-11-17 10:20:40 +0900

    Feat: MealCard 컴포넌트 구현
    당류, 단백질, 탄수화물, 지방, 칼로리 등 식사와 관련된 정보를 표시해주는 Card입니다.

---
+++ frontend/src/components/molecules/MealCard.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import DefaultCard from '@/styles/Card';
+import { rWidth } from '@/utils/style';
+import TextWithSmallTitle from '../atoms/TextWithSmallTitle';
+
+const CardSection = styled(DefaultCard)`
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+const CardContent = styled.View``;
+
+const CardHeaderBox = styled.View`
+  width: ${rWidth(280)}px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const CalorieTextWrapper = styled.Text`
+  ${({ theme }) => theme.typographys.p1r}
+  color : ${({ theme }) => theme.colors.primary};
+`;
+
+const CardBodyBox = styled.View`
+  width: ${rWidth(280)}px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+interface MealCardProps {
+  mode: 'day' | 'meal';
+  count?: number;
+  time?: string;
+  amount?: number;
+  foodName?: string;
+  calorie: number;
+  sugar: number;
+  protein: number;
+  carbohydrate: number;
+  fat: number;
+}
+
+export default function MealCard({
+  mode,
+  count,
+  time,
+  amount,
+  foodName,
+  calorie,
+  sugar,
+  protein,
+  carbohydrate,
+  fat,
+}: MealCardProps) {
+  return (
+    <CardSection size="lg">
+      <CardContent>
+        <CardHeaderBox>
+          <TextWithSmallTitle
+            title={mode === 'day' ? `${count}회` : `${amount}g`}
+            text={mode === 'day' ? `${time}` : `${foodName}`}
+          />
+          <CalorieTextWrapper>{`${calorie}Kcal`}</CalorieTextWrapper>
+        </CardHeaderBox>
+        <CardBodyBox>
+          <TextWithSmallTitle
+            title="당류"
+            text={`${Math.round(sugar).toString()}g`}
+          />
+          <TextWithSmallTitle
+            title="단백질"
+            text={`${Math.round(protein).toString()}g`}
+          />
+          <TextWithSmallTitle
+            title="탄수화물"
+            text={`${Math.round(carbohydrate).toString()}g`}
+          />
+          <TextWithSmallTitle
+            title="지방"
+            text={`${Math.round(fat).toString()}g`}
+          />
+        </CardBodyBox>
+      </CardContent>
+    </CardSection>
+  );
+}
