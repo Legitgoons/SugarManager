c6dc4d13bbe23d5987884f67b94ef7ae398e46b8
이의찬 <cksxkr5193@naver.com>
2023-11-20 08:18:27 +0900

    Feat: BloodSugar Redux 구현
    BloodSugarDetail 조회를 위해 날짜를 Redux에 전역 상태로 저장하도록 했습니다.

--- frontend/src/redux/store/rootReducer.ts
+++ frontend/src/redux/store/rootReducer.ts
@@ -3,18 +3,22 @@ import { persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import userSliceReducer from '../slice/userSlice';
 import navigationSliceReducer from '../slice/navigationSlice';
+import timeReducer from '../slice/bloodSugarSlice';
 
 const persistSetting = {
   key: 'root',
   storage: AsyncStorage,
   version: 0,
-  whitelist: ['user', 'navigation'],
+  whitelist: ['user', 'navigation', 'time'],
   blacklist: [],
 };
+
 const combinedReducers = combineReducers({
   user: userSliceReducer,
   navigation: navigationSliceReducer,
+  time: timeReducer,
 });
+
 const rootReducer = persistReducer(persistSetting, combinedReducers);
 
 export default rootReducer;
