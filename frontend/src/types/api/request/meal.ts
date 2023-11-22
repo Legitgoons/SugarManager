785d896e93780937d5a6648fb784abdedae294d5
이의찬 <cksxkr5193@naver.com>
2023-11-22 14:30:46 +0900

    Feat: fetch Meal에 사용되는 type을 interface를 이용하여 구현

--- frontend/src/types/api/request/meal.ts
+++ frontend/src/types/api/request/meal.ts
@@ -24,3 +24,18 @@ export interface MealSave {
   foodSalt: number;
   foodSugars: number;
 }
+
+export interface PeriodMealData {
+  dayFoodCal: number;
+  dayFoodSugars: number | null;
+  dayFoodProtein: number | null;
+  dayFoodCarbohydrate: number | null;
+  dayFoodFat: number | null;
+  time: string;
+}
+
+export interface PeriodResponseData {
+  success: boolean;
+  response: PeriodMealData[];
+  error: any;
+}
