eaa1af1d5dba7d0beb97aac9bd4742c80224fbfe
이의찬 <cksxkr5193@naver.com>
2023-11-24 17:08:39 +0900

    Feat: mealSlice.ts 구현
    redux를 이용해 MealDailyScreen에 사용되는 mealTime과 MealDetailScreen에 사용되는 mealPK를 전역으로 선언하였습니다.

--- frontend/src/redux/store/rootReducer.ts
+++ frontend/src/redux/store/rootReducer.ts
@@ -4,6 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import userSliceReducer from '../slice/userSlice';
 import navigationSliceReducer from '../slice/navigationSlice';
 import timeReducer from '../slice/bloodSugarSlice';
+import mealReducer from '../slice/mealSlice';
 
 const persistSetting = {
   key: 'root',
@@ -17,6 +18,7 @@ const combinedReducers = combineReducers({
   user: userSliceReducer,
   navigation: navigationSliceReducer,
   time: timeReducer,
+  meal: mealReducer,
 });
 
 const rootReducer = persistReducer(persistSetting, combinedReducers);
