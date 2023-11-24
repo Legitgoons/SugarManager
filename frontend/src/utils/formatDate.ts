31d9c0d98bae26de5a697faacc38b30bde661343
이의찬 <cksxkr5193@naver.com>
2023-11-24 17:35:08 +0900

    Feat: formatToTime 함수 추가 구현 및 formatToApiDateTime 함수 이름 수정

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -1,4 +1,4 @@
-export function formatToFullDateTime(date: Date) {
+export function formatToApiDateTime(date: Date) {
   const year = date.getFullYear();
   const month = `0${date.getMonth() + 1}`.slice(-2);
   const day = `0${date.getDate()}`.slice(-2);
@@ -14,6 +14,16 @@ export function formatToMonthDay(time: string) {
   const day = date.getDate();
 
   return `${month.toString().padStart(2, '0')}/${day
+    .toString()
+    .padStart(2, '0')}`;
+}
+
+export function formatToTime(time: string) {
+  const date = new Date(time);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  return `${hours.toString().padStart(2, '0')}:${minutes
     .toString()
     .padStart(2, '0')}`;
 }
