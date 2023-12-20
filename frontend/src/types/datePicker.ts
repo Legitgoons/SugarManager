8b511f36b611cb26ef0188cc106a1e438f0a1e7e
Uichan Lee <cksxkr5193@naver.com>
2023-12-20 18:12:15 +0900

    Fix: DatePickerMode type 분리

--- frontend/src/types/datePicker.ts
+++ frontend/src/types/datePicker.ts
@@ -1,12 +1,14 @@
+export type DatePickerMode = 'datetime' | 'date' | 'time';
+
 /**
  * @param {Date} date 날짜 상태
  * @param {(newDate: Date) => void} setDate 날짜 setter
- * @param {typeof mode} mode 날짜와 시간 선택 여부
+ * @param {DatePickerMode} mode 날짜와 시간 선택 여부
  */
 
 export interface DatePickerProps {
   date: Date;
   // eslint-disable-next-line no-unused-vars
   setDate: (newDate: Date) => void;
-  mode: 'datetime' | 'date' | 'time';
+  mode: DatePickerMode;
 }
